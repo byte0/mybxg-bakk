c31eabc51a9a9897d5bd28a1a12ba21a9b9338d9
code1031 <wangzijie1031@qq.com>
2017-06-09 14:57:28 +0800

    完成讲师查看功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -17,6 +17,10 @@ define(['jquery','template','bootstrap'],function($,template){
                     data : {tc_id : tcId},
                     dataType : 'json',
                     success : function(data){
+                        // data.result.tc_hometown = data.result.tc_hometown.replace(/[|]/g,' ');
+                        // data.result.tc_hometown = data.result.tc_hometown.replace(/\|/g,' ');
+                        data.result.tc_hometown = data.result.tc_hometown.split('|').join(' ');
+                        console.log(data.result.tc_hometown);
                         var html = template('teacherModalInfoTpl',data.result);
                         $('#teacherModalInfo').html(html);
                         // 显示弹窗
