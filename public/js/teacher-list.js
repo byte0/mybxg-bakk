0f6f0a44a4c813ee8e6896698dd853a0199f7495
code1031 <wangzijie1031@qq.com>
2017-06-09 15:18:05 +0800

    实现讲师注销和启用功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -8,30 +8,61 @@ define(['jquery','template','bootstrap'],function($,template){
             // 解析数据并渲染页面
             var html = template('teacherInfoTpl',{list:data.result});
             $('#teacherInfo').html(html);
-            // 绑定查看讲师信息的单击事件
-            $('#teacherInfo').find('.preview').click(function(){
-                var tcId = $(this).closest('td').attr('data-id');
-                $.ajax({
-                    type : 'get',
-                    url : '/api/teacher/view',
-                    data : {tc_id : tcId},
-                    dataType : 'json',
-                    success : function(data){
-                        // data.result.tc_hometown = data.result.tc_hometown.replace(/[|]/g,' ');
-                        // data.result.tc_hometown = data.result.tc_hometown.replace(/\|/g,' ');
-                        data.result.tc_hometown = data.result.tc_hometown.split('|').join(' ');
-                        console.log(data.result.tc_hometown);
-                        var html = template('teacherModalInfoTpl',data.result);
-                        $('#teacherModalInfo').html(html);
-                        // 显示弹窗
-                        $('#teacherModal').modal();
-                    }
-                });
-                return false;
-            });
+            // 查看讲师功能
+            previewTeacher();
+            // 注销和启用讲师
+            enableOrDisableTeacher();
         }
     });
 
-          
+    // 查看讲师功能
+    function previewTeacher(){
+        // 绑定查看讲师信息的单击事件
+        $('#teacherInfo').find('.preview').click(function(){
+            var tcId = $(this).closest('td').attr('data-id');
+            $.ajax({
+                type : 'get',
+                url : '/api/teacher/view',
+                data : {tc_id : tcId},
+                dataType : 'json',
+                success : function(data){
+                    // data.result.tc_hometown = data.result.tc_hometown.replace(/[|]/g,' ');
+                    // data.result.tc_hometown = data.result.tc_hometown.replace(/\|/g,' ');
+                    data.result.tc_hometown = data.result.tc_hometown.split('|').join(' ');
+                    console.log(data.result.tc_hometown);
+                    var html = template('teacherModalInfoTpl',data.result);
+                    $('#teacherModalInfo').html(html);
+                    // 显示弹窗
+                    $('#teacherModal').modal();
+                }
+            });
+            return false;
+        });
+    }
+    // 注销和启用讲师
+    function enableOrDisableTeacher(){
+        $('#teacherInfo').find('.edteacher').click(function(){
+            var that = this;
+            var td = $(this).closest('td');
+            var tcId = td.attr('data-id');
+            var tcStatus = td.attr('data-status');
+            $.ajax({
+                type : 'post',
+                url : '/api/teacher/handle',
+                data : {tc_id : tcId,tc_status : tcStatus},
+                dataType : 'json',
+                success : function(data){
+                    if(data.code == 200){
+                        td.attr('data-status',data.result.tc_status);
+                        if(data.result.tc_status == 0){
+                            $(that).text('注销');
+                        }else{
+                            $(that).text('启用');
+                        }
+                    }
+                }
+            });
+        });
+    }
 
 });
