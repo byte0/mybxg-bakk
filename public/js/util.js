b082e35a44f6df9e6dc5db28e0a9205522dee9c3
code1031 <wangzijie1031@qq.com>
2017-06-10 10:06:45 +0800

    实现根据id查询值的功能

--- public/js/util.js
+++ public/js/util.js
@@ -1,8 +1,39 @@
 define(['jquery'],function($){
+
+    // var setMenu = function(pathname){
+    //     $('.aside .navs a').removeClass('active');
+    //     $('.aside .navs a[href="'+pathname+'"]').addClass('active');
+    // }
+
+    // var abc = function(){
+    //     console.log('hello');
+    // }
+
+    // var obj = {
+    //     setMenu : setMenu,
+    //     abc : abc
+    // }
+
+    // return obj;
     return {
         setMenu : function(pathname){
             $('.aside .navs a').removeClass('active');
             $('.aside .navs a[href="'+pathname+'"]').addClass('active');
+        },
+        qs : function(attr,param){
+            // ?abc=123&flag=456
+            var p = param.substring(1);
+            var arr = p.split('&');
+            var retValue = '';
+            arr.forEach(function(element,index){
+                var kv = element.split('=');
+                if(attr == kv[0]){
+                    retValue = kv[1];
+                    // return的作用是结束循环
+                    return;
+                }
+            });
+            return retValue;
         }
     }
 });
