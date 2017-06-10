87d12697ff191ec5a7526da6b000bc95e3e8b2c8
code1031 <wangzijie1031@qq.com>
2017-06-10 11:17:40 +0800

    完成添加和编辑讲师功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,20 +1,26 @@
 define(['jquery','util','template'],function($,util,template){
     // 设置导航选中
     util.setMenu('/teacher/list');
-    // 提交表单处理
-    $('#addTeacherBtn').click(function(){
-        $.ajax({
-            type : 'post',
-            url : '/api/teacher/add',
-            data : $('#addTeacherForm').serialize(),
-            dataType : 'json',
-            success : function(data){
-                console.log(data);
-            }
-        });
-    });
     // 获取参数中tc_id
     var tcId = util.qs('tc_id',location.search);
+
+    // 提交表单处理
+    function submitForm(url){
+        $('#addTeacherBtn').click(function(){
+            $.ajax({
+                type : 'post',
+                url : url,
+                data : $('#addTeacherForm').serialize(),
+                dataType : 'json',
+                success : function(data){
+                    if(data.code == 200){
+                        location.href = '/teacher/list';
+                    }
+                }
+            });
+        });
+    }
+    
     if(tcId){
         // 编辑讲师操作
         // 根据ID查询数据
@@ -28,13 +34,15 @@ define(['jquery','util','template'],function($,util,template){
                 data.result.operateFlag = '编辑';
                 var html = template('teacherFormTpl',data.result);
                 $('#teacherFormInfo').html(html);
+                submitForm('/api/teacher/update');
             }
         });
     }else{
         // 添加讲师操作
         $('#navFlag').html('讲师添加');
-        var html = template('teacherFormTpl',{operateFlag:'添加'});
+        var html = template('teacherFormTpl',{operateFlag:'添加',tc_gender:1});
         $('#teacherFormInfo').html(html);
+        submitForm('/api/teacher/add');
     }
     
 
